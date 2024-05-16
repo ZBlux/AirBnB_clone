b464eff7db8ff8e43de92bd2a5a68ec6acf8241f
ZBlux <aminekamili@hotmail.com>
2024-05-16 15:48:53 -0700

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'update_at']:
-                        value = date.time.strptime(
+                        value = datetime.strptime(
                                 value, '%Y-%m-%dT%H:%M:%S.%f'
                                 )
                         setattr(self, key, value)
